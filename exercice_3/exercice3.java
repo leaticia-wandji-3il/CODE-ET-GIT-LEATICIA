032fb4b3b41de6a376c0a4ace22844f67bf51632
Leaticia Wandji <wandjisl@3il.fr>
2024-11-26 14:54:32 +0100

    Correction de l'exercice3

--- exercice_3/exercice3.java
+++ exercice_3/exercice3.java
@@ -1,22 +1,52 @@
+/**
+ * Classe StockManager
+ * Cette classe permet de gérer le stock d'un produit en fonction des opérations d'ajout et de retrait
+ */
 public class StockManager {
 
-    // gere le stock
+    /**
+     * Gère le stock en fonction du type d'opération.
+     * 
+     * @param typeOperation Le type d'opération ("ajout" ou "retrait")
+     * @param produit       Le nom du produit
+     * @param quantite      La quantité à ajouter ou retirer
+     * @param stock         Le stock actuel du produit
+     */
     public void gererStock(String typeOperation, String produit, int quantite, int stock) {
-        // Ajout au stock
         if (typeOperation.equals("ajout")) {
-            stock += quantite;
-            System.out.println("Produit : " + produit + ", Stock après ajout : " + stock);
-        // Retrait du stock
+            ajouterStock(produit, quantite, stock);
         } else if (typeOperation.equals("retrait")) {
-            if (stock >= quantite) {
-                stock -= quantite;
-                System.out.println("Produit : " + produit + ", Stock après retrait : " + stock);
-            } else {
-                System.out.println("Stock insuffisant pour le produit : " + produit);
-            }
-        // Si l'action est inconnu
+            retirerStock(produit, quantite, stock);
         } else {
             System.out.println("Opération inconnue.");
+        }
+    }
+
+    /**
+     * Ajoute la quantité spécifiée au stock.
+     * 
+     * @param produit  Le nom du produit
+     * @param quantite La quantité à ajouter
+     * @param stock    Le stock actuel du produit
+     */
+    private void ajouterStock(String produit, int quantite, int stock) {
+        stock += quantite;
+        System.out.println("Produit : " + produit + ", Stock après ajout : " + stock);
+    }
+
+    /**
+     * Retire la quantité spécifiée du stock si possible.
+     * 
+     * @param produit  Le nom du produit
+     * @param quantite La quantité à retirer
+     * @param stock    Le stock actuel du produit
+     */
+    private void retirerStock(String produit, int quantite, int stock) {
+        if (stock >= quantite) {
+            stock -= quantite;
+            System.out.println("Produit : " + produit + ", Stock après retrait : " + stock);
+        } else {
+            System.out.println("Stock insuffisant pour le produit : " + produit);
         }
     }
 }
